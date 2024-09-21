bfd58c22b51039b7eae514d3081f5033c264c1b3
garvitarunav <garvitarunavltd@gmail.com>
2024-09-22 01:49:17 +0530

    Update WEBAPP.py

--- WEBAPP.py
+++ WEBAPP.py
@@ -1,19 +1,14 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import joblib
 import webbrowser
-import os
+import pickle
 
-# Check if the model file exists
-if not os.path.exists('iris_model.joblib'):
-    st.error("Model file not found!")
-else:
-    # Load the model
-    RFC = joblib.load('iris_model.joblib')
-    print("Model loaded:", type(RFC))  # Check the type
+with open('iris_model.joblib', 'rb') as file:
+    RFC = pickle.load(file)
 
-columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    
+columns = ['sepal_length','sepal_width','petal_length','petal_width']
 
 def predict():
     try:
@@ -31,15 +26,19 @@ def predict():
     except Exception as e:
         st.error(f"Error in prediction: {e}")
 
-# Sidebar inputs
-st.sidebar.title("USER INPUTS")
-sepal_length = st.sidebar.slider("Sepal Length (cm)", 4.30, 7.90)
-sepal_width = st.sidebar.slider("Sepal Width (cm)", 2.20, 4.40)
-petal_length = st.sidebar.slider("Petal Length (cm)", 1.00, 6.90)
-petal_width = st.sidebar.slider("Petal Width (cm)", 0.10, 2.50)
-st.sidebar.button('SUBMIT', on_click=predict)
+    # st.balloons()
 
-# Main page content
+
+#### SIDEBAR ####
+
+st.sidebar.title("USER INPUTS")
+sepal_length = st.sidebar.slider("Sepal Length (cm)",4.30,7.90)
+sepal_width = st.sidebar.slider("Sepal Width (cm)",2.20,4.40)
+petal_length = st.sidebar.slider("Petal Length (cm)",1.00,6.90)
+petal_width = st.sidebar.slider("Petal Length (cm)",0.10,2.50)
+st.sidebar.button('SUBMIT',on_click=predict)
+
+#### MAIN PAGE ####
 input_data = pd.DataFrame({
     'sepal_length_(cm)': [sepal_length],
     'sepal_width_(cm)': [sepal_width],
@@ -47,11 +46,11 @@ input_data = pd.DataFrame({
     'petal_width_(cm)': [petal_width]
 })
 
-expected_data = pd.DataFrame({
-    'Species': ['SETOSA', 'VERSICOLOR', 'VIRGINICA'],
-    'Label': [0, 1, 2]
+expected_data = ({
+    'Sesota':0,
+    'Versicolor':1,
+    'Virginica':2
 })
-
 st.markdown("# Welcome Home! Discover More, Dream Bigger! :book:")
 st.subheader("Input Features")
 st.dataframe(input_data)
@@ -61,3 +60,6 @@ st.dataframe(expected_data)
 
 if st.button("More on this"):
     webbrowser.open_new_tab("https://www.linkedin.com/posts/garvit-gupta-87875a226_data-pre-processing-activity-7162410464151322624-AhdR?utm_source=share&utm_medium=member_desktop")
+
+
+
